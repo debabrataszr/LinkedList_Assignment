a87881bd08863c0eea54b682dc1e9daa833cd585
debabrata-97 <debabrataszr@gmail.com>
2023-03-10 12:47:30 +0530

    Added: Code to Sort the Linked List in Ascending Order

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        MyLinkedList object = new MyLinkedList();
+        object.addToFront(myFirstNode);
+        object.appendSort(myFirstNode,mySecondNode);
+        object.appendSort(mySecondNode,myThirdNode);
+
+        MyNode<Integer> mynewNode = new MyNode<>(40);
+        object.insertAfterParticular(mySecondNode,mynewNode);
+
+
+        object.printMyNodes();
+    }
+}
